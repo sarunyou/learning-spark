f6d38d6b66443ae473efcf6bc84f1cc40e9b31f4
Sarunyou (Tee) <swhan@sertiscorp.com>
2022-02-26 19:40:13 +0700

    feat: update countDistinct

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType
@@ -7,41 +6,9 @@ from pyspark.sql.types import StructField
 from pyspark.sql.types import StructType
 
 if __name__ == "__main__":
-	conf = SparkConf()
-	conf.setAll(
-			[
-					(
-							"spark.master",
-							os.environ.get("SPARK_MASTER_URL"),
-					),
-					("spark.app.name", "simple-app"),
-			]
-	)
-
-	spark = SparkSession.builder.appName("Sample App").config("spark.master", os.environ.get("SPARK_MASTER_URL", "spark://spark:7077")).getOrCreate()
-	rows = [
-			[1, 100],
-			[2, 200],
-			[3, 300],
-	]
-
-	schema = StructType(
-			[
-					StructField("id", IntegerType(), True),
-					StructField("salary", IntegerType(), True),
-			]
-	)
-
-	df = spark.createDataFrame(rows, schema=schema)
-
-	highest_salary = df.agg({"salary": "max"}).collect()[0]["max(salary)"]
-
-	second_highest_salary = (
-			df.filter(f"`salary` < {highest_salary}")
-			.orderBy("salary", ascending=False)
-			.select("salary")
-			.limit(1)
-	)
-
-	second_highest_salary.show()
+	logFile = "/test-files/mock.csv"  # Should be some file on your system
+	spark = SparkSession.builder.appName("UniqueUsers").config("spark.master", os.environ.get("SPARK_MASTER_URL", "spark://spark:7077")).getOrCreate()
+	logData = spark.read.text(logFile).cache()
+	countDistinct = logData.distinct().count()
+	print("countDistinct", countDistinct)
 	spark.stop()
