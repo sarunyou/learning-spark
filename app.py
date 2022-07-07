96fe2afff9213894f7d4c3ed3eea13e1a1d5bdc7
Sarunyou (Tee) <swhan@sertiscorp.com>
2022-07-08 00:47:19 +0700

    refactor: remove unused file

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 import os
 from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.types import IntegerType
-from pyspark.sql.types import StructField
 from pyspark.sql.types import StructType
+from pyspark.sql import functions as F
 
+
+def foreach_batch_function(df, epoch_id):
+    file_name = df.first()["name"]
+    print("file_name", file_name)
+    df = df.dropDuplicates(["id"])
+    df.select(["id", "fId"]).show()
 if __name__ == "__main__":
-	logFile = "/test-files/mock.csv"  # Should be some file on your system
-	spark = SparkSession.builder.appName("UniqueUsers").config("spark.master", os.environ.get("SPARK_MASTER_URL", "spark://spark:7077")).getOrCreate()
-	schema = StructType().add("id", "string")
-	csvDf = spark.read.schema(schema).csv(logFile)
-	countDistinct = csvDf.groupBy(csvDf.id).count().select(csvDf.id)
-	countDistinct.show(n=10)
-	print("countDistinct", countDistinct.count())
-	spark.stop()
+    path = f"/test-files/input"
+    schema = StructType().add("id", "string").add("fId", "string")
+    conf = SparkConf().setAll([
+        ("spark.master", os.environ.get("SPARK_MASTER_URL", "spark://spark:7077"))
+    ])
+    spark = SparkSession.builder.appName(
+        "UniqueUsers").config(conf=conf).getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    csvDf = spark.readStream.option("sep", ",").schema(
+        schema).option("maxFilesPerTrigger", 1).csv(path)
+    csvDf = csvDf.withColumn("path", F.input_file_name())
+    csvDf = csvDf.withColumn("path_splitted", F.split("path", "/"))
+    csvDf = csvDf.withColumn("name", F.col(
+        "path_splitted").getItem(F.size("path_splitted")-1))
+    csvDf.writeStream.format("console").foreachBatch(foreach_batch_function).outputMode(
+        "update").start().awaitTermination()
--- gcp/start-pubsub.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-gcloud config set project ${PUBSUB_PROJECT_ID}
-
-gcloud beta emulators pubsub start \
-  --data-dir=/opt/data \
-  --host-port=${PUBSUB_LISTEN_ADDRESS}
